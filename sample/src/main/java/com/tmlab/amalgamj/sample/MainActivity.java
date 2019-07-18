49a99a2664dac447c93890938e0950cfa35aea51
Yuri Zykov <yuri.zykov@gmail.com>
2019-07-18 10:11:50 +0600

    Migrate to Android X

--- sample/src/main/java/com/tmlab/amalgamj/sample/MainActivity.java
+++ sample/src/main/java/com/tmlab/amalgamj/sample/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tmlab.amalgamj.sample;
 
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.tmlab.amalgamj.Amalgam;
 import com.tmlab.amalgamj.Connection;
